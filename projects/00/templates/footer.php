5ab8d98087c30aba93a605cd98f1aa0294319156
karsonwest <karsonwest@mail.weber.edu>
2024-10-01 15:52:20 +0000

    created project 00 folder

---
+++ projects/00/templates/footer.php
@@ -0,0 +1,15 @@
+</main>
+    <!-- END MAIN PAGE CONTENT-->
+
+    <!-- BEGIN PAGE FOOTER-->
+     <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>My Webpage</strong> by <a href="#">Karson West</a>. The source code is licensed under MIT.
+            </p>
+        </div>
+     </footer>
+     <!-- END PAGE FOOTER -->
+</body>
+
+</html>
